1eb4414d473653c467824e10ee70472abfb4d469
XanderPSON <xwpeterson@gmail.com>
2016-01-04 19:32:56 -0800

    Add Events show view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :ratings
   get 'home/team'
   get 'home/map'
+  get 'events/map'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
