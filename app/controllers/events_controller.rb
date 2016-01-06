ed3d5ccf74b99743aecc177e290fac07cf681238
XanderPSON <xwpeterson@gmail.com>
2016-01-05 17:18:00 -0800

    Remove new event path test

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -35,9 +35,7 @@ class EventsController < ApplicationController
   # GET /events/new
   def new
     @event = Event.new
-    p "9" * 2000
     if request.xhr?
-    p "10" * 2000
       respond_to do |format|
         format.html {render layout: false}
       end
